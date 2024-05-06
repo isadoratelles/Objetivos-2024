8f4a79b9fa7a73d887a955664aa110703540e2d7
Isadora Telles <128058321+isadoratelles@users.noreply.github.com>
2024-05-06 08:33:30 -0300

    Add files via upload

--- objetivos 2024/main.js
+++ objetivos 2024/main.js
@@ -2,6 +2,9 @@ const botoes = document.querySelectorAll(".botao");
 const textos=document.querySelectorAll(".aba-conteudo");
 const contadores=document.querySelectorAll(".contador");
 const tempoObjetivo1=new Date("2024-12-31T23:59");
+const tempoObjetivo2=new Date("2024-12-31T23:59");
+const tempoObjetivo3=new Date("2024-12-31T23:59");
+const tempoObjetivo4=new Date("2024-12-31T23:59");
 const tempoAtual= new Date;
 //Declara quais elementos vão ser lidos. O script vai ler todos que tiverem a classe "botao".
 
@@ -19,4 +22,23 @@ for(let i=0;i <botoes.length;i++){
     }
 }
 
-contadores[0].textContent=tempoObjetivo1-tempoAtual;
+contadores[0].textContent=calculaTempo(tempoObjetivo1);
+contadores[1].textContent=calculaTempo(tempoObjetivo2);
+contadores[2].textContent=calculaTempo(tempoObjetivo3);
+contadores[3].textContent=calculaTempo(tempoObjetivo4);
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual=new Date();
+    let tempoFinal=tempoObjetivo-tempoAtual;
+    let segundos=Math.floor(tempoFinal/1000);
+    let minutos=Math.floor(segundos/60);
+    let horas=Math.floor(minutos/60);
+    let dias=Math.floor(horas/24);
+
+    segundos%=60;
+    minutos%=60;
+    horas%=24;
+
+    return dias+"dias"+horas+"horas"+minutos+"minutos"+segundos+"segundos";
+}
+
